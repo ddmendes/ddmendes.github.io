ba88d4d85c4b3193c8cd28fff0673f317fb9828e
Davi Diorio Mendes <ddioriomendes@gmail.com>
2015-12-19 11:51:05 -0200

    Some interations to banner buttons.

--- js/ddmendes.js
+++ js/ddmendes.js
@@ -1,5 +1,41 @@
 $(document).ready(function() {
-    $('#btn_en').show();
-    $('#btn_resume').show();
-    $('#btn_download').show();
+    $('#btn_en').show().click(btn_en_click);
+    $('#btn_resume').show().click(btn_resume_click);
+    $('#btn_download').show().click(btn_download_click);
+
+    $('#btn_pt').click(btn_pt_click);
+    $('#btn_code').click(btn_code_click);
+    $('#btn_download').click(btn_download_click);
 });
+
+function btn_en_click() {
+    $('#btn_en').hide();
+    $('#btn_pt').show();
+
+    return false;
+}
+
+function btn_pt_click() {
+    $('#btn_en').show();
+    $('#btn_pt').hide();
+
+    return false;
+}
+
+function btn_resume_click() {
+    $('#btn_resume').hide();
+    $('#btn_code').show();
+
+    return false;
+}
+
+function btn_code_click() {
+    $('#btn_resume').show();
+    $('#btn_code').hide();
+
+    return false;
+}
+
+function btn_download_click() {
+    return false;
+}
